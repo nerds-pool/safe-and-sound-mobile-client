3ad72b6974977655b1edc08cb3250163fe0ea4e2
kaveenhyacinth <alwiskaveen@gmail.com>
2021-04-05 05:37:48 +0530

    screen setup

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Safe&Sound by Nerds Pool Inc.</Text>
       <StatusBar style="auto" />
+      <Text>Safe&Sound by Nerds Pool Inc.</Text>
     </View>
   );
 }
