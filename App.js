1740017fcc6f262c96956968b0e75b81838b54de
kaveenhyacinth <alwiskaveen@gmail.com>
2021-04-05 07:54:03 +0530

    Config header buttons

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <MainNavigation isGuest={true} isPHI={false} />
+      <MainNavigation isGuest={false} isPHI={false} />
     </NavigationContainer>
   );
 }
--- src/screens/common/homeScreen.js
+++ src/screens/common/homeScreen.js
@@ -1,14 +1,79 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect, useCallback } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../../components/ui/customHeaderButton";
+import Colors from "../../theme/Colors";
 
-const homeScreen = () => {
+const homeScreen = (props) => {
+  const [Type, setType] = useState("user");
+
+  useEffect(() => {
+    repaintHeaderButtons();
+  }, [repaintHeaderButtons]);
+
+  const repaintHeaderButtons = useCallback(() => {
+    if (Type === "user") {
+      props.navigation.setOptions({
+        headerRight: () => (
+          <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item
+              title="History"
+              IconComponent={Ionicons}
+              iconName="document-text-outline"
+              color={Colors.accent}
+              onPress={() => {}}
+            />
+            <Item
+              title="Log Out"
+              IconComponent={Ionicons}
+              iconName="log-out-outline"
+              color={Colors.accent}
+              onPress={() => {}}
+            />
+          </HeaderButtons>
+        ),
+      });
+    }
+
+    if (Type === "phi") {
+      props.navigation.setOptions({
+        headerRight: () => (
+          <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item
+              title="Add Test Resut"
+              IconComponent={Ionicons}
+              iconName="create-outline"
+              color={Colors.accent}
+              onPress={() => {}}
+            />
+            <Item
+              title="Search"
+              IconComponent={Ionicons}
+              iconName="search-outline"
+              color={Colors.accent}
+              onPress={() => {}}
+            />
+            <Item
+              title="Log Out"
+              IconComponent={Ionicons}
+              iconName="log-out-outline"
+              color={Colors.accent}
+              onPress={() => {}}
+            />
+          </HeaderButtons>
+        ),
+      });
+    }
+  }, [Type]);
+
   return (
     <View>
       <Text>Home screen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default homeScreen
+export default homeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
