1986db7e1d0ea5341e6bab36bfdc5872eded17e4
kaveenhyacinth <alwiskaveen@gmail.com>
2021-04-07 17:33:50 +0530

    add search user screen ui design

---
+++ src/dummy-data/users.js
@@ -0,0 +1,8 @@
+export default [
+  { id: 1, name: "Kaveen De Alwis" },
+  { id: 2, name: "Kavinda Thennakoon" },
+  { id: 3, name: "Shameera Carrim" },
+  { id: 4, name: "Suchith Fernando" },
+  { id: 5, name: "Tharusha Kudagala" },
+  { id: 6, name: "Yashen Samarasinghe" },
+];
--- src/screens/phi/searchUserScreen.js
+++ src/screens/phi/searchUserScreen.js
@@ -1,14 +1,60 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Dimensions,
+  Pressable,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import theme from "../../theme";
+import Screen from "../../components/ui/Screen";
+import Users from "../../dummy-data/users";
 
-const searchUserScreen = () => {
+const { width: WINDOW_WIDTH } = Dimensions.get("window");
+
+const UserResultTile = ({ name, onNavigate }) => {
   return (
-    <View>
-      <Text>Search user</Text>
-    </View>
-  )
-}
+    <Pressable onPress={onNavigate}>
+      <View style={styles.tileContainer}>
+        <Text>{name}</Text>
+        <Ionicons name="chevron-forward-outline" size={16} />
+      </View>
+    </Pressable>
+  );
+};
 
-export default searchUserScreen
+const searchUserScreen = (props) => {
+  return (
+    <Screen>
+      <TextInput
+        style={theme.styles.txtInput}
+        placeholder="Enter NIC to Search a User"
+      />
+      {Users.map((user) => (
+        <UserResultTile
+          key={Users.indexOf(user)}
+          name={user.name}
+          onNavigate={() => props.navigation.navigate("user", { id: user.id })}
+        />
+      ))}
+    </Screen>
+  );
+};
 
+export default searchUserScreen;
 
+const styles = StyleSheet.create({
+  tileContainer: {
+    alignItems: "center",
+    borderColor: theme.colors.background,
+    borderWidth: 1,
+    flexDirection: "row",
+    height: 40,
+    justifyContent: "space-between",
+    marginVertical: 2,
+    paddingHorizontal: 10,
+    width: WINDOW_WIDTH - 30,
+  },
+});
